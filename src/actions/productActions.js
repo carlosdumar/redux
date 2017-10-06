a78bac34a1d20970959c1db1061973d0224aa497
Carlos Andres Rodriguez <carlosrodriguez@velocitypartners.net>
2017-10-06 09:52:04 -0500

    lo mas

---
+++ src/actions/productActions.js
@@ -0,0 +1,88 @@
+import {
+    FETCH_PRODUCTS_SUCCESS,
+    FETCH_PRODUCTS_FAILURE,
+    FETCH_PRODUCT_SUCCESS,
+    FETCH_PRODUCT_FAILURE,
+    SAVE_PRODUCT_SUCCESS,
+    SAVE_PRODUCT_FAILURE
+} from 'types';
+
+import API from '../api';
+
+export function fetchProductsSuccess (products) {
+    return {
+        type: FETCH_PRODUCTS_SUCCESS,
+        payload: products
+    }
+}
+
+export function fetchProductsFailure (error) {
+    return {
+        type: FETCH_PRODUCTS_FAILURE,
+        payload: error
+    };
+}
+
+export function fetchProductSuccess (product) {
+    return {
+        type: FETCH_PRODUCT_SUCCESS,
+        payload: products
+    };
+}
+
+export function fetchProductFailure (error) {
+    return {
+        type: FETCH_PRODUCT_FAILURE,
+        payload: error
+    };
+}
+
+export function saveProductSuccess (product) {
+    return {
+        type: SAVE_PRODUCT_SUCCESS,
+        payload: product
+    };
+}
+
+export function fetchProductFailure (error) {
+    return {
+        type: SAVE_PRODUCT_FAILURE,
+        payload: error
+    };
+}
+
+// Actions Creators (Async)
+export function fetchProducts() {
+    return async (dispatch => {
+        dispatch(() => {
+            return {
+                type: 'FETCH_PRODUCT_INIT'
+            }
+        })
+
+        try {
+            const data = await API.products.getAll();
+            return dispatch(fetchProductsSuccess(data.products))
+        } catch(error) {
+            return dispatch(fetchProductsFailure(error));
+        }
+    });
+}
+
+export function fetchProduct(productId  ) {
+    return async (dispatch => {
+        dispatch(() => {
+            return {
+                type: 'FETCH_PRODUCT_INIT'
+            };
+        });
+
+        try {
+            const data = await API.products.getSingle(productId);
+        }
+    })
+}
+
+export function saveProduct() {
+
+}
